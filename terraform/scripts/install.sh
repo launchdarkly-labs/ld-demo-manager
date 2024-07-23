613e6d4598e5d3d7e809eca864720a566179d318
Kevin Cochran <40193276+kevincloud@users.noreply.github.com>
2024-07-22 21:32:17 -0400

    update

--- terraform/scripts/install.sh
+++ terraform/scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt -y update
-apt -y install pipx python3 python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+apt -y install pipx python3 python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools nginx
 ln -s /usr/bin/python3 /usr/bin/python
 
 mkdir -p /opt/secrets
@@ -17,3 +17,25 @@ pip install -r requirements.txt --break-system-packages
 pip install wheel --break-system-packages
 pip install gunicorn --break-system-packages
 pip install cachelib --break-system-packages
+
+cat > /etc/systemd/system/demo-manager.service <<-EOF
+[Unit]
+Description=Demo Manager
+After=network.target
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=root
+WorkingDirectory=/opt/ld-demo-manager
+ExecStart=/usr/local/bin/gunicorn --config config.py manager:app
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable demo-manager
+systemctl start demo-manager
+

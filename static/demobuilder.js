4ca79adf74324f0ea6e7b2de51e7588854b6f050
Kevin Cochran <40193276+kevincloud@users.noreply.github.com>
2024-09-23 20:12:27 -0400

    update

--- static/demobuilder.js
+++ static/demobuilder.js
@@ -37,6 +37,8 @@ function demobuilder(email) {
                 document.getElementById("project_name").innerHTML = "Project Name: <a class=\"has-text-link-40\" href=\"https://app.launchdarkly.com/projects/" + res.body.projectKey + "/flags\" target=\"_blank\">" + res.body.projectName + "</a>";
                 document.getElementById("client_id").innerHTML = "Client ID: " + res.body.clientId;
                 populateExp(res.body.sdkKey, res.body.projectKey);
+                runEvals(projectKey);
+                enableBuild();
             } else {
                 document.getElementById("current_status").innerHTML = "There was an error building the demo project."
                 document.getElementById("error_message").innerHTML = this.responseText;
@@ -54,18 +56,18 @@ function populateExp(sdkKey, projectKey) {
     document.getElementById("current_status").innerHTML = "Populating Experiment with data...";
     xhr.open("POST", url, true);
     xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            if (this.status == 200) {
-                var res = JSON.parse(this.responseText);
-                runEvals(projectKey);
-            } else {
-                document.getElementById("current_status").innerHTML = "There was an error populating the experiment with data."
-                document.getElementById("error_message").innerHTML = this.responseText;
-                enableBuild();
-            }
-        }
-    }
+    // xhr.onreadystatechange = function () {
+    //     if (this.readyState == 4) {
+    //         if (this.status == 200) {
+    //             var res = JSON.parse(this.responseText);
+    //             runEvals(projectKey);
+    //         } else {
+    //             document.getElementById("current_status").innerHTML = "There was an error populating the experiment with data."
+    //             document.getElementById("error_message").innerHTML = this.responseText;
+    //             enableBuild();
+    //         }
+    //     }
+    // }
     xhr.send(JSON.stringify({ "sdk_key": sdkKey, "num_iterations": 1047 }));
 }
 
@@ -75,19 +77,19 @@ function runEvals(projectKey) {
     document.getElementById("current_status").innerHTML = "Evaluating flags...";
     xhr.open("POST", url, true);
     xhr.setRequestHeader('Content-type', 'application/json');
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            if (this.status == 200) {
-                var res = JSON.parse(this.responseText);
-                document.getElementById("current_status").innerHTML = "Done!";
-            } else {
-                document.getElementById("current_status").innerHTML = "Done!";
-                // document.getElementById("current_status").innerHTML = "There was an error evaluating flags."
-                // document.getElementById("error_message").innerHTML = this.responseText;
-            }
-            enableBuild();
-        }
-    }
+    // xhr.onreadystatechange = function () {
+    //     if (this.readyState == 4) {
+    //         if (this.status == 200) {
+    //             var res = JSON.parse(this.responseText);
+    //             document.getElementById("current_status").innerHTML = "Done!";
+    //         } else {
+    //             document.getElementById("current_status").innerHTML = "Done!";
+    //             // document.getElementById("current_status").innerHTML = "There was an error evaluating flags."
+    //             // document.getElementById("error_message").innerHTML = this.responseText;
+    //         }
+    //         enableBuild();
+    //     }
+    // }
     xhr.send(JSON.stringify({ "project_key": projectKey, "num_iterations": 35 }));
 }
 
